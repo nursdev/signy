4dd8a1519871362d5edfa2e3791db56928a0d83b
Nursultan <stanbek1004@gmail.com>
2022-12-19 00:05:07 +0600

    updated slider in main page

--- script.js
+++ script.js
@@ -148,3 +148,35 @@ function changeInt(el) {
   }
   //dotnumber.classList.contains("active-dot")
 }
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlide(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlideShow");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+
+  dots[slideIndex - 1].className += " active";
+  slides[slideIndex - 1].style.display = "flex";
+}
